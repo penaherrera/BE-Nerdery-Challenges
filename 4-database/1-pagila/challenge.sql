8e63d2cd4b7856233cd842d269157d2dab8ee670
penaherrera <carlos.cpa26@gmail.com>
2025-05-02 16:50:11 -0600

    adding query solutions for pagila db challenges

--- 4-database/1-pagila/challenge.sql
+++ 4-database/1-pagila/challenge.sql
@@ -7,7 +7,17 @@
     - film_count should show the total number of films in that category
     - Results should be grouped by category name
  */
-
+SELECT
+    category.name AS category,
+    COUNT(film_category.film_id) AS film_count
+FROM 
+    category
+INNER JOIN 
+    film_category ON category.category_id = film_category.category_id
+GROUP BY
+    category.category_id, category.name
+ORDER BY
+    film_count DESC;
 
 -- your query here
 
@@ -23,11 +33,23 @@
 
  -- your query here
 
-
-
+SELECT 
+    customer.first_name,
+    customer.last_name,
+    SUM(amount) AS total_spent
+FROM
+    customer
+INNER JOIN
+    payment ON customer.customer_id = payment.customer_id
+GROUP BY
+    customer.first_name, customer.last_name
+ORDER BY
+    total_spent DESC, customer.first_name ASC
+LIMIT 5;
 
 /*
-    Challenge 3.
+    Challenge 3. 
+    Note: MOVIES THAT HAVE BEEN RENTED
     Write a SQL query that lists all film titles that have not been rented in the past 10 years in the Pagila database.
     - The query should return one column: title
     - title should display the name of each film that hasn't been rented
@@ -37,7 +59,22 @@
 
 
 -- your query here
-
+SELECT 
+    f.title,
+	r.rental_date
+FROM 
+    film AS f
+LEFT JOIN 
+    inventory AS i ON f.film_id = i.film_id
+LEFT JOIN 
+    rental AS r ON i.inventory_id = r.inventory_id 
+    AND r.rental_date >= '2014-01-01'
+GROUP BY 
+    f.title, r.rental_date
+HAVING 
+    COUNT(r.rental_id) > 0
+ORDER BY 
+    f.title;
 
 /*
     Challenge 4.
@@ -50,7 +87,20 @@
 
 -- your query here
 
-
+SELECT 
+    f.title,
+    i.inventory_id
+FROM 
+    film AS f
+LEFT JOIN 
+    inventory AS  i ON f.film_id = i.film_id
+LEFT JOIN 
+    rental AS r ON i.inventory_id = r.inventory_id
+WHERE 
+    r.rental_id IS NULL
+    AND i.inventory_id IS NOT NULL
+ORDER BY 
+    f.title;
 
 
 /*
@@ -61,6 +111,27 @@
     - rental_count should show the total number of times the film was rented
 */
 
+SELECT 
+    fr.title,
+    fr.rental_count
+FROM (
+    SELECT
+        f.title,
+        COUNT(r.rental_id) AS rental_count,
+        AVG(COUNT(r.rental_id)) OVER() AS avg_rental_count
+    FROM 
+        film AS f
+    LEFT JOIN 
+        inventory AS i ON f.film_id = i.film_id
+    LEFT JOIN 
+        rental AS r ON i.inventory_id = r.inventory_id
+    GROUP BY
+        f.film_id, f.title
+) AS fr
+WHERE 
+    fr.rental_count > fr.avg_rental_count
+ORDER BY
+    fr.rental_count DESC;
 
 
 -- your query here
@@ -76,6 +147,30 @@
 */
 
 -- your query here
+WITH customer_rentals AS (
+    SELECT
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        r.rental_date
+    FROM
+        rental AS r
+    INNER JOIN
+        customer AS c ON r.customer_id = c.customer_id
+)
+
+SELECT
+    first_name,
+    last_name,
+    MIN(rental_date) AS first_rental,
+    MAX(rental_date) AS last_rental,
+    (MAX(rental_date) - MIN(rental_date)) AS rental_span_days
+FROM
+    customer_rentals
+GROUP BY
+    customer_id, first_name, last_name
+ORDER BY
+    rental_span_days DESC;
 
 /*
     Challenge 7.
@@ -86,6 +181,26 @@
 
 
 -- your query here
+WITH all_genres AS (
+    SELECT COUNT(DISTINCT category_id) AS total_genres
+    FROM film_category
+)
+
+SELECT 
+    c.first_name,
+    c.last_name
+FROM 
+    customer c
+WHERE 
+    (SELECT COUNT(DISTINCT fc.category_id)
+     FROM rental r
+     JOIN inventory i ON r.inventory_id = i.inventory_id
+     JOIN film_category fc ON i.film_id = fc.film_id
+     WHERE r.customer_id = c.customer_id
+    ) < (SELECT total_genres FROM all_genres)
+ORDER BY 
+    c.last_name, c.first_name;
+
 
 /*
     Bonus Challenge 8 (opt)
